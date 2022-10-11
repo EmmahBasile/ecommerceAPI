9687445b7789cba6820b719d5c01040253757654
EmmahBasile <ajebasile2@gmail.com>
2022-10-11 07:33:45 +0100

    first commit

---
+++ adds/serializers.py
@@ -0,0 +1,47 @@
+from dataclasses import fields
+from importlib.metadata import files
+from rest_framework import serializers
+from .models import Category, Book, Product
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'id',
+            'title'
+        )
+        model = Category
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'id',
+            'title',
+            'category',
+            'isbn',
+            'pages',
+            'price',
+            'stock',
+            'description',
+            'imageURL',
+            'status',
+            'date_created'
+        )
+        model = Book
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'id',
+            'product_tag',
+            'name',
+            'category',
+            'price',
+            'stock',
+            'imageURL',
+            'status',
+            'date_created'
+        )
+        model = Product
